9d2d279303de55babaab7150ab57ec4192358e75
Saad Mughal <49732416+Saadmughall@users.noreply.github.com>
2022-03-11 16:36:18 +0500

    first commit

---
+++ build/js.js
@@ -0,0 +1,26 @@
+// Menu button
+$(document).ready(function () {
+    $(".nav-toggler").each(function (_, navToggler) {
+      var target = $(navToggler).data("target");
+      $(navToggler).on("click", function () {
+        $(target).animate({
+          height: "toggle",
+        });
+      });
+    });
+  });
+// if you want to change color of navbar so un comment
+// var a = $("nav").offset().top;
+
+$(document).scroll(function(){
+    if($(this).scrollTop() > 100)
+    {   
+       $('nav').css({"background":"rgb(250 204 21)",
+       "transition-timing-function":" ease-in-out ",
+       "transition-duration": ".4s",
+       "transition-delay": ".1.2s"
+    });
+    } else {
+       $('nav').css({"background":"#000"});
+    }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily:{
+      sans:['Aclonica', 'sans-serif'],
+      sans1:['Poppins', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
